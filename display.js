0c18921bef9cb452ff5635779dee8992a247b30e
John Goodman <johncurtisgoodman@gmail.com>
2018-04-10 22:36:30 -0400

    Assign card positions based on canvas height.

--- display.js
+++ display.js
@@ -1,23 +1,27 @@
-var canvas;
-var ctx;
-var cards = [];
-var opponentCards = [];
-var currentCards = [];
-var currentOpponentCards = [];
-var focusIndex = 5;
-var positions;
+var canvas
+var canvasHeight
+var ctx
+var cards = []
+var opponentCards = []
+var currentCards = []
+var currentOpponentCards = []
+var focusIndex = 5
+var positions
 
 onload = () => {
     //*//
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-    cards = generateCards(drawCards.bind(null, canvas, ctx, cards), 'player');
-    opponentCards = generateCards(drawCards.bind(null, canvas, ctx, opponentCards), 'opponent');
-    initTouchControls();
+    canvas = document.getElementById('canvas')
+    canvas.style.transform = `translateY(${(window.innerHeight - canvas.getBoundingClientRect().height)/2}px)`
+    positions.player.riverStart.y = canvas.height - 165
+    positions.opponent.riverStart.y = 15
+    ctx = canvas.getContext('2d')
+    cards = generateCards(drawCards.bind(null, canvas, ctx, cards), 'player')
+    opponentCards = generateCards(drawCards.bind(null, canvas, ctx, opponentCards), 'opponent')
+    initTouchControls()
     setTimeout(drawCards.bind(null, canvas, ctx, cards), 1000)
     currentCards = cards.map(card => { return card })
     currentOpponentCards = opponentCards.map(card => { return card })
-};
+}
 
 positions = {
   player: {
@@ -44,7 +48,7 @@ positions = {
     },
     riverStart: {
       x: 0,
-      y: 480,
+      y: 400,
       z: 1,
       height: 150,
       width: 108,
@@ -71,14 +75,14 @@ positions = {
 }
 
 var Card = function (image, name) {
-    this.image = new Image ();
-    this.name = name;
-    this.imageLoaded = false;
+    this.image = new Image ()
+    this.name = name
+    this.imageLoaded = false
     this.image.onload = () => {
-      this.imageLoaded = true;
+      this.imageLoaded = true
     }
-    this.image.src = image;
-};
+    this.image.src = image
+}
 
 var clearCanvas = (canvas, ctx) => {
   ctx.clearRect(
@@ -86,7 +90,7 @@ var clearCanvas = (canvas, ctx) => {
     0,
     canvas.width,
     canvas.height
-  );
+  )
 }
 
 var drawCards = (canvas, ctx, cards) => {
@@ -97,126 +101,126 @@ var drawCards = (canvas, ctx, cards) => {
             card.y,
             card.width,
             card.height,
-        );
-    });
+        )
+    })
 }
 
 var generateCards = (onCardLoad, holder) => {
-    var aristocrat;
-    var deck = [];
-    var imageLoadTrigger;
+    var aristocrat
+    var deck = []
+    var imageLoadTrigger
     //*//
-    king = new Card ('./images/king.png', 'king');
-    general = new Card ('./images/general.png', 'general');
-    philosopher = new Card ('./images/philosopher.png', 'philosopher');
-    falseProphet = new Card ('./images/false-prophet.png', 'false prophet');
-    aristocrat = new Card ('./images/aristocrat.png', 'aristocrat');
-    revolutionary = new Card ('./images/revolutionary.png', 'revolutionary');
-    reverse = new Card ('./images/reverse.png', 'reverse');
-    deck = [revolutionary, general, falseProphet, philosopher, aristocrat, king];
+    king = new Card ('./images/king.png', 'king')
+    general = new Card ('./images/general.png', 'general')
+    philosopher = new Card ('./images/philosopher.png', 'philosopher')
+    falseProphet = new Card ('./images/false-prophet.png', 'false prophet')
+    aristocrat = new Card ('./images/aristocrat.png', 'aristocrat')
+    revolutionary = new Card ('./images/revolutionary.png', 'revolutionary')
+    reverse = new Card ('./images/reverse.png', 'reverse')
+    deck = [revolutionary, general, falseProphet, philosopher, aristocrat, king]
     deck.map((card, index) => {
-        card.x = positions[holder].center.x;
-        card.y = positions[holder].center.y;
-        card.z = positions[holder].center.z;
-        card.height = positions[holder].center.height;
-        card.width = positions[holder].center.width;
-    });
+        card.x = positions[holder].center.x
+        card.y = positions[holder].center.y
+        card.z = positions[holder].center.z
+        card.height = positions[holder].center.height
+        card.width = positions[holder].center.width
+    })
     imageLoadTrigger = {
         loadCount: 0,
         finishedCount: deck.length,
         event: onCardLoad,
-    };
+    }
     imageLoadTrigger.progress = function () {
-        this.loadCount += 1;
+        this.loadCount += 1
         if (this.loadCount == this.finishedCount) {
-            this.event();
+            this.event()
         }
-    }.bind(imageLoadTrigger);
+    }.bind(imageLoadTrigger)
     deck.forEach((card) => {
-        card.image.onload = imageLoadTrigger.progress;
-    });
-    return deck;
-};
+        card.image.onload = imageLoadTrigger.progress
+    })
+    return deck
+}
 
 var moveTo = (card, x, y, width, height) => {
-    var int;
-    var frame;
-    var totalFrames;
-    var xDistance;
-    var yDistance;
-    var widthDifference;
-    var heightDifference;
+    var int
+    var frame
+    var totalFrames
+    var xDistance
+    var yDistance
+    var widthDifference
+    var heightDifference
     //*//
-    width = width ? width : card.width;
-    height = height ? height : card.height;
-    frame = 0;
-    totalFrames = 8;
-    xDistance = Math.abs(card.x - x);
-    yDistance = Math.abs(card.y - y);
-    widthDifference = Math.abs(card.width - width);
-    heightDifference = Math.abs(card.height - height);
+    width = width ? width : card.width
+    height = height ? height : card.height
+    frame = 0
+    totalFrames = 8
+    xDistance = Math.abs(card.x - x)
+    yDistance = Math.abs(card.y - y)
+    widthDifference = Math.abs(card.width - width)
+    heightDifference = Math.abs(card.height - height)
     int = window.setInterval(() => {
-        frame += 1;
-        card.x += (xDistance / totalFrames) * (card.x < x ? 1 : -1);
-        card.y += (yDistance / totalFrames) * (card.y < y ? 1 : -1);
-        card.width += (widthDifference / totalFrames) * (card.width > x ? 1 : -1);
-        card.height += (heightDifference / totalFrames) * (card.height > y ? 1 : -1);
+        frame += 1
+        card.x += (xDistance / totalFrames) * (card.x < x ? 1 : -1)
+        card.y += (yDistance / totalFrames) * (card.y < y ? 1 : -1)
+        card.width += (widthDifference / totalFrames) * (card.width > x ? 1 : -1)
+        card.height += (heightDifference / totalFrames) * (card.height > y ? 1 : -1)
         if (frame >= totalFrames) {
-          window.clearInterval(int);
-          card.width = width;
-          card.height = height;
+          window.clearInterval(int)
+          card.width = width
+          card.height = height
         }
-        clearCanvas(canvas, ctx);
-        drawCards(canvas, ctx, currentCards);
-        drawCards(canvas, ctx, currentOpponentCards);
-    }, 32);
+        clearCanvas(canvas, ctx)
+        drawCards(canvas, ctx, currentCards)
+        drawCards(canvas, ctx, currentOpponentCards)
+    }, 32)
 }
 
 var updateDeckDisplay = () => {
     cards.forEach((card, index) => {
         if (index < focusIndex) {
-          moveTo(card, positions.player.offleft.x, positions.player.offleft.y, positions.player.offleft.width, positions.player.offleft.height);
+          moveTo(card, positions.player.offleft.x, positions.player.offleft.y, positions.player.offleft.width, positions.player.offleft.height)
         } else if (index > focusIndex) {
-          moveTo(card, positions.player.offright.x, positions.player.offright.y, positions.player.offright.width, positions.player.offright.height);
+          moveTo(card, positions.player.offright.x, positions.player.offright.y, positions.player.offright.width, positions.player.offright.height)
         } else {
-          moveTo(card, positions.player.center.x, positions.player.center.y, positions.player.center.width, positions.player.center.height);
+          moveTo(card, positions.player.center.x, positions.player.center.y, positions.player.center.width, positions.player.center.height)
         }
-    });
+    })
 }
 
 var zoomOut = () => {
-    var pos;
+    var pos
     cards.forEach((card, index) => {
-        pos = positions.player.riverStart;
-        moveTo(card, pos.x + (index * pos.fan), pos.y, pos.width, pos.height);
-    });
+        pos = positions.player.riverStart
+        moveTo(card, pos.x + (index * pos.fan), pos.y, pos.width, pos.height)
+    })
     opponentCards.forEach((card, index) => {
-        pos = positions.opponent.riverStart;
-        moveTo(card, pos.x + (index * pos.fan), pos.y, pos.width, pos.height);
-    });
+        pos = positions.opponent.riverStart
+        moveTo(card, pos.x + (index * pos.fan), pos.y, pos.width, pos.height)
+    })
 }
 
 window.swipeLeft = () => {
-    focusIndex += focusIndex < cards.length - 1 ? 1 : 0;
-    updateDeckDisplay();
-};
+    focusIndex += focusIndex < cards.length - 1 ? 1 : 0
+    updateDeckDisplay()
+}
 
 window.swipeRight = () => {
-    focusIndex -= focusIndex > 0 ? 1 : 0;
-    updateDeckDisplay();
-};
+    focusIndex -= focusIndex > 0 ? 1 : 0
+    updateDeckDisplay()
+}
 
 window.swipeUp = () => {
-    play(cards[focusIndex].name);
+    play(cards[focusIndex].name)
     currentOpponentCards = opponentCards.filter(card => {
         return opponent.hand.cards.includes(card.name)
     })
     currentCards = cards.filter(card => {
         return player.hand.cards.includes(card.name)
     })
-    zoomOut();
-};
+    zoomOut()
+}
 
 window.swipeDown = () => {
-    updateDeckDisplay();
-};
+    updateDeckDisplay()
+}
